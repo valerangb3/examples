ca7d4897629ee26acf88190316880e9313142089
Valery <kare@34web.ru>
2023-09-06 12:59:00 +0300

    Правки от 06.09 13:00 стр.53

--- t1.php
+++ t1.php
@@ -1,16 +1,37 @@
 <?php
 class ShopProduct {
     public $title = "Стандартный товар";
-    public $productName = "Фамилия автора";
+    public $producerMainName = "Фамилия автора";
     public $producerFirstName = "Имя автора";
     public $price = 0;
+
+    function __construct(
+        $title,
+        $firstName,
+        $mainName,
+        $price
+    ) {
+        $this->title = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName = $mainName;
+        $this->price = $price;
+    }
 
     function __toString() {
         return "Hello world!\n";
     }
+
+    public function getProducer() {
+        return $this->producerFirstName . " "
+            . $this->producerMainName;
+    }
 }
 
-$sp1 = new ShopProduct();
-$sp2 = new ShopProduct();
-var_dump($sp1);
-var_dump($sp2);
+$product1 = new ShopProduct(
+    "Собачье сердце",
+    "Михаил",
+    "Булгаков",
+    5.99
+);
+
+print "Автор: {$product1->getProducer()}\n";
